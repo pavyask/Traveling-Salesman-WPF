583b21a7f4e5965073f7179a1732cd2f84e3270a
pavyask <pavel.yasko03@gmail.com>
2023-01-06 17:43:50 +0100

    Interface draft

---
+++ TSP/TSP-WPF/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TSP_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void browseFile_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void startButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void stopButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void pauseButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
